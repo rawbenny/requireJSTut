97f8f6eac4e167c9805cb620a1bce76841203b6b
Ryan Yan <rawbenny@gmail.com>
2014-01-06 12:16:24 +0800

    version 3

--- js/main.js
+++ js/main.js
@@ -1,8 +1,12 @@
 require.config({
-    baseUrl: 'js'
+	baseUrl: 'js'
 });
- 
-require(['selector'], function(query) {
-    var els = query('#wrapper');
-    console.log(els);
+
+require(['selector', 'event'], function($, E) {
+	var els = $('p');
+	for (var i=0; i<els.length; i++) {
+		E.bind(els[i], 'click', function() {
+			alert(this.innerHTML);
+		});
+	}
 });
